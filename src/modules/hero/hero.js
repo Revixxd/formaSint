0b2eb460acbe96ad0bede7e761f5658ec761fe36
Adam Myslicki <revixxdm@gmail.com>
2025-06-09 22:09:31 +0200

    [HERO] Implement hero section with responsive design and image integration

---
+++ src/modules/hero/hero.js
@@ -0,0 +1,26 @@
+import '../../styles/hero.css';
+import { subscribeDeviceStatus } from '../../utils/getDeviceStatus.js';
+
+// TODO: Make h1 more figma designed
+
+const hero = document.querySelector('.hero');
+
+function updateHero({ isMobile, isTablet, isDesktop }) {
+  hero.classList.toggle('hero-mobile', isMobile);
+  hero.classList.toggle('hero-tablet', isTablet);
+  hero.classList.toggle('hero-desktop', isDesktop);
+
+  if (isMobile || isTablet) {
+    hero.innerHTML = `
+      <h1 class="hero-title">Forma’sint.</h1>
+      <img src="/src/assets/hero/hero-img.webp" alt="Hero Image" class="hero-image" />
+    `;
+  } else {
+    hero.innerHTML = `
+      <h1 class="hero-title">Forma’sint.</h1>
+      <img src="/src/assets/hero/hero-img.webp" alt="Hero Image" class="hero-image" />
+    `;
+  }
+}
+
+subscribeDeviceStatus(updateHero);
