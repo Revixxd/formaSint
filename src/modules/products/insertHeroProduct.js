61a68bd6ec2109dc8abdc0fc3e20a4efe73a7577
Adam Myslicki <revixxdm@gmail.com>
2025-06-10 12:21:44 +0200

    [FEATURE] Add hero product section with responsive design and styling

---
+++ src/modules/products/insertHeroProduct.js
@@ -0,0 +1,34 @@
+function insertHeroProduct(container, index) {
+  const heroProduct = document.createElement('div');
+
+  heroProduct.classList.add('product-hero');
+  heroProduct.classList.add('product-element');
+
+  heroProduct.innerHTML = `
+    <div class="product-hero__content">
+      <div class="product-hero__content__up-container">
+        <p>Forma’sint.</p>
+        <h2>You'll look and feel like the champion.</h2>
+      </div>
+      <div class="product-hero__content__down-container">
+        <button class="btn btn--primary">Check this out</button>
+      </div>
+    </div>
+    <div class="hero-product__image-container">
+      <img src="https://placehold.co/1350x500" alt="Hero Product Image" />
+    </div>
+  `;
+
+  if (window.innerWidth >= 1024) {
+    heroProduct.style.gridColumn = '2 / span 2';
+    heroProduct.style.gridRow = `${Math.floor(index / 4) + 1}`;
+  } else {
+    heroProduct.style.gridColumn = '1 / span 2';
+    // TODO: Adjust for mobile layout -> bigger hero product (2-3 rows)
+    heroProduct.style.gridRow = `${Math.floor(index / 4) * 2 + 1} / span 3`;
+  }
+
+  container.appendChild(heroProduct);
+}
+
+export default insertHeroProduct;
--- src/modules/products/products.js
+++ src/modules/products/products.js
@@ -1,75 +1,92 @@
 import '../../styles/products.css';
 import getProducts from '../../utils/getProducts.js';
+import { subscribeDeviceStatus } from '../../utils/getDeviceStatus.js';
+import insertHeroProduct from './insertHeroProduct.js';
 
-const hero = document.querySelector('.products');
+function updateProducts({ isMobile, isTablet, isDesktop }) {
+  const products = document.querySelector('.products');
 
-hero.innerHTML = `
-  <div class="products__select-container">
-    <p>Number of products per page</p>
-    <select id="per-page">
-      <option value="14">14</option>
-      <option value="24">24</option>
-      <option value="36">36</option>
-    </select>
-  </div>
-  <div class="products-list"></div>
-  <div id="loader" style="display: none;">Loading...</div>
-`;
+  products.classList.toggle('products-mobile', isMobile);
+  products.classList.toggle('products-tablet', isTablet);
+  products.classList.toggle('products-desktop', isDesktop);
 
-const productList = document.querySelector('.products-list');
-const perPageSelect = document.getElementById('per-page');
-const loader = document.getElementById('loader');
+  products.innerHTML = `
+    <div class="products__select-container">
+      <p>Number of products per page</p>
+        <select id="per-page">
+          <option value="14">14</option>
+          <option value="24">24</option>
+          <option value="36">36</option>
+        </select>
+    </div>
+      <div class="products-list"></div>
+      <div id="loader" style="display: none;">Loading...</div>
+  `;
 
-let perPage = parseInt(perPageSelect.value, 14);
-let currentIndex = 0;
-let isLoading = false;
+  const productList = document.querySelector('.products-list');
+  const perPageSelect = document.getElementById('per-page');
+  const loader = document.getElementById('loader');
 
-function generateProduct(product) {
-  const div = document.createElement('div');
-  div.classList.add('product-item');
+  let perPage = parseInt(perPageSelect.value, 14);
+  let currentIndex = 0;
+  let isLoading = false;
 
-  div.innerHTML = `
+  function generateProduct(product) {
+    const div = document.createElement('div');
+    div.classList.add('product-item');
+    div.classList.add('product-element');
+
+    div.innerHTML = `
       <span class="product-item__id">ID: ${product.id}</span>
       <div class="product-item__img-container">
         <img src="${product.image}" alt="${product.name}" />
       </div>
     `;
 
-  return div;
-}
-
-async function loadProducts() {
-  if (isLoading) return;
-  isLoading = true;
-  loader.style.display = 'block';
-
-  try {
-    const data = await getProducts(currentIndex / perPage + 1, perPage);
-    data.products.forEach((product) => {
-      const productItem = generateProduct(product);
-      productList.appendChild(productItem);
-    });
-
-    currentIndex += data.products.length;
-  } catch (error) {
-    console.error('Error loading products:', error);
-  } finally {
-    isLoading = false;
-    loader.style.display = 'none';
+    return div;
   }
-}
 
-perPageSelect.addEventListener('change', () => {
-  perPage = parseInt(perPageSelect.value, 10);
-  productList.innerHTML = '';
-  currentIndex = 0;
-  loadProducts();
-});
+  async function loadProducts() {
+    if (isLoading) return;
+    isLoading = true;
+    loader.style.display = 'block';
 
-window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+    try {
+      const data = await getProducts(currentIndex / perPage + 1, perPage);
+      let oneHeroInserted = false;
+      data.products.forEach((product, index) => {
+        const productItem = generateProduct(product);
+        productList.appendChild(productItem);
+        let productRow = Math.floor((currentIndex + index) / 4) + 1;
+        if ((index + 1) % 4 === 0 && !oneHeroInserted && productRow % 2 === 0) {
+          oneHeroInserted = true;
+          insertHeroProduct(productList, index);
+        }
+      });
+
+      currentIndex += data.products.length;
+    } catch (error) {
+      console.error('Error loading products:', error);
+    } finally {
+      isLoading = false;
+      loader.style.display = 'none';
+    }
+  }
+
+  perPageSelect.addEventListener('change', () => {
+    perPage = parseInt(perPageSelect.value, 10);
+    productList.innerHTML = '';
+    currentIndex = 0;
     loadProducts();
-  }
-});
+  });
 
-loadProducts();
+  window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+      loadProducts();
+    }
+  });
+
+  loadProducts();
+}
+
+subscribeDeviceStatus(updateProducts);
